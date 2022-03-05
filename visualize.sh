8361b4a16c07f506b60e0171f5eb117f460cb16d
April Zhao <abooli@outlook.com>
2022-03-04 22:10:00 -0800

    Added all

--- visualize.sh
+++ visualize.sh
@@ -1,4 +1,4 @@
-VARS="#코로나바이러스 #コロナウイルス #冠状病毒 #covid2019 #covid-2019 #covid19 #covid-19 #coronavirus #corona #virus #flu #sick #cough #sneeze #hospital #nurse #doctor"
+VARS="_all #코로나바이러스 #コロナウイルス #冠状病毒 #covid2019 #covid-2019 #covid19 #covid-19 #coronavirus #corona #virus #flu #sick #cough #sneeze #hospital #nurse #doctor"
 for HASHTAG in $VARS; do
     ./src/visualize.py --input_path=reduced.country --key=$HASHTAG | head > viz/$HASHTAG.country;
     ./src/visualize.py --input_path=reduced.lang --key=$HASHTAG | head > viz/$HASHTAG.lang;
